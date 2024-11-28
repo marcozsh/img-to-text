f9b501b0ed8f763948b3ca4cc601e63093eef69a
marcozsh <marc.penar@outlook.cl>
2024-11-28 00:42:28 -0300

    Squash merge upload-file-functionality into dev

--- src/data/blog-content.ts
+++ src/data/blog-content.ts
@@ -1,5 +1,23 @@
 export const blogContent = [
   {
+    title: "¿Cómo funciona?",
+    content: `
+    	<p>
+    	1. Caputura una imagen (Windows + Shift + S) 
+	</p>
+    	<br> 
+    	<p>2. Puedes hacer 1 de estas 3 acciones:</p> 
+	<ul style="list-style:disc;padding-left:40px">
+		<li> Puedes Guadarla para luego arrastrarla hasta el cuadro rojo</li>
+		<li> Puedes copiar la imagen dentro del cuadro rojo (CTRL + V)</li>
+		<li> Puedes subir la imagen presionando "Subir una imagen"</li>
+	   </ul>
+    	<br> 
+    	<p>
+	3. Por último, puedes editar a tu gusto y copiar el texto transformado con el botón "Copiar al clipboard"
+	</p>`,
+  },
+  {
     title: "¿Qué es Tesseract.js?",
     content: `<p>
     	Tesseract.js es una biblioteca de JavaScript que permite realizar reconocimiento óptico de caracteres (OCR, por sus siglas en inglés) 
@@ -14,21 +32,6 @@ export const blogContent = [
 	El OCR (Optical Character Recognition) es una tecnología que permite convertir texto en imágenes 
   	(como documentos escaneados, capturas de pantalla o fotografías) en texto editable o procesable. 
 	Por ejemplo, puedes tomar una imagen de un cartel y extraer el texto que contiene para almacenarlo o procesarlo.
-	</p>`,
-  },
-  {
-    title: "¿Cómo funciona?",
-    content: `
-    	<p>
-    	1. Caputura una imagen (Windows + Shift + S) 
-	</p>
-    	<br> 
-    	<p>
-	2. Luego, puedes guardarla y arrastrala al cuadro rojo o puedes simplemente pegarla en este mismo (CTRL + V)
-	</p> 
-    	<br> 
-    	<p>
-	3. Por último, puedes copiar el texto transformado con el botón \"Copiar al clipboard\"
 	</p>`,
   },
 ];
